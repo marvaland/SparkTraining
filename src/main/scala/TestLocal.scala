699332de2358d593f8fca1575474dd754ba8d2fe
Mahendra Maid <mahendramaid@gmail.com>
2018-07-12 02:23:10 +0530

    First Commit

---
+++ src/main/scala/TestLocal.scala
@@ -0,0 +1,29 @@
+package main.scala
+
+import org.apache.spark.sql.SparkSession
+import java.util.concurrent.Executors
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future, _}
+import scala.util.control.NonFatal
+
+object TestLocal {
+
+  def main(args: Array[String]) {
+
+
+
+
+    val conf:SparkConf = new SparkConf().setAppName("Histogram").setMaster("local[*]")
+    val sc:SparkContext = new SparkContext(conf)
+
+
+
+    println("TestSuccessfull");
+  }
+}
+
